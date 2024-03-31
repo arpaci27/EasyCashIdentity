7b7b18f07197962712c64dbe7a7add49a62a1c8a
Ömer Arpacı <91760786+arpaci27@users.noreply.github.com>
2024-03-31 18:31:40 +0300

    Repositories added.

---
+++ EasyCashIdentity.DataLayer/Abstract/ICustomerAccountProcessDal.cs
@@ -0,0 +1,13 @@
+﻿using EasyCashIdentity.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentity.DataAccesLayer.Abstract
+{
+	public interface ICustomerAccountProcessDal:IGenericDal<CustomerAccount>
+	{
+	}
+}
---
+++ EasyCashIdentity.DataLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentity.DataAccesLayer.Abstract
+{
+	public interface IGenericDal<T> where T : class
+	{
+		void Insert(T t);
+		T Delete(T t);
+		void Update(T t);
+		T GetByID(int id);
+		List<T> GetList();
+	}
+}
